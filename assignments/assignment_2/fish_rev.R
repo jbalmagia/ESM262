d6d9a9f7cabc912d4a9d14ea829192945add9d81
jbalmagia <jbalmagia@gmail.com>
2020-03-07 14:01:12 -0800

    initial commit - made sample data and got started on function

---
+++ assignments/assignment_2/fish_rev.R
@@ -0,0 +1,15 @@
+#'Revenue from fish catch by fishery and location
+#'
+#'This function calculates the frequency of fish species caught by location, total revenue by location, and total revenue by fishery
+#'
+#'@param price a that has prices for different fish
+#'@param catch a table that has the number caught for each fish species for each location, where locations are columns and fish are rows
+#'@return frequent the most frequently caught fish in each location
+#'@return rev_location the total revenue of all fisheries by location
+#'@return rev_fish the total revenue of each fishery across all locations
+
+
+
+fish_rev = function(price, catch){
+  
+}
