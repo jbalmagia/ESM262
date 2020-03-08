2bb4cc38eb9ce38ce7c09487883fbfccf9ad6717
Jessica Jagdeo <jessica32703@gmail.com>
2020-03-08 12:12:17 -0700

    Working on other nested functions

--- assignments/assignment_2/fish_rev.R
+++ assignments/assignment_2/fish_rev.R
@@ -2,7 +2,7 @@
 #'
 #'This function calculates the frequency of fish species caught by location, total revenue by location, and total revenue by fishery
 #'
-#'@param price a that has prices for different fish
+#'@param price a data frame that has prices for different fish
 #'@param catch a table that has the number caught for each fish species for each location, where locations are columns and fish are rows
 #'@return frequent the most frequently caught fish in each location
 #'@return rev_location the total revenue of all fisheries by location
@@ -24,15 +24,28 @@ fish_info = function(catch,price){
   return(max_by_loc)
   
 #loc_revenue() is a nested function within fish_info that finds total revenue one location
-  loc_revenue = function(catch, price){
-    loc_price = 
-  }
+   loc_revenue = function(catch, price){
+     loc_rev = summary(catch) * price
+     return(loc_rev)
+   }
+   
+# Use sapply() to find total revenue at each location in catch table
+   rev_by_loc = sapply(catch, loc_revenue)
+   return(rev_by_loc)
+   
+# fish_revenue() is a nested function within fish_info that finds total revenue for each fish species across locations
+   fish_revenue = function(catch, price) {
+     fish_rev = 
+   }
   
-#Use sapply() to find total revenue at each location in catch table
+# Use sapply() to find total revenue for each fish species
+   rev_by_fish = sapply(catch, fish_revenue)
+   return(rev_by_fish)
   
 
 #Jessica - Now we need to make 2 more nested functions, one for revenue by location and one for revenue by fish species
-
+   
+ return(list(max_by_loc, loc_price, rev_by_loc))
 
 }
 
