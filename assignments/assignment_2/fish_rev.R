3936cdd07d705b93ad3379c663192b10d3082286
Jessica Jagdeo <jessica32703@gmail.com>
2020-03-09 16:00:48 -0700

    Fixed most frequent fish calculation

--- assignments/assignment_2/fish_rev.R
+++ assignments/assignment_2/fish_rev.R
@@ -12,37 +12,34 @@
 
 fish_rev = function(catch,price, graph = FALSE){
   
-#fish_max finds most freq fish caught for one location
-   fish_max = as.factor(catch)
-   fish_max = names(which.max(summary(catch)))
+# fish_max finds most freq fish caught for each location
+   # fish_max = as.factor(catch)
+   # fish_max = names(which.max(summary(catch)))
+   fish_max = list(colnames(catch), rownames(catch)[apply(catch, 2, which.max)])
   
-#Use sapply() to find most freq fish caught at each location in catch table
+# Use sapply() to find most freq fish caught at each location in catch table
    # max_by_loc = sapply(catch, fish_max)
    # return(max_by_loc)
   
 # total_revenue finds the total revenue at all locations
    total_revenue = sum(price[,1]*catch)
    
-#loc_revenue finds total revenue at each location
+# loc_revenue finds total revenue at each location
    loc_revenue = price[,1]*catch
    loc_revenue = colSums(loc_revenue)
    loc_revenue_df = as.data.frame(loc_revenue)
-   loc_revenue_df = colnames(c("location", "loc_revenue"))
-  colnames(test) <- (c("loc_revenue"))
+   loc_revenue_df = rownames_to_column(loc_revenue_df, "location")
+  # loc_revenue_df = colnames(c("location", "loc_revenue"))
+  # colnames(test) <- (c("loc_revenue"))
    
 # fish_revenue finds total revenue for each fish species across locations
    fish_revenue = price[,1]*catch
    fish_revenue = rowSums(fish_revenue)
    
-#Jenny - I created the remaining equations. To output them, I had to get rid of the nested functions. I also created a new data frame with fish counts by locations (fish_loc), but the fish_max equation isn't working properly now. Sorry! Trying to troubleshoot.
-   
-# I also created the graph, but it won't run because we need to figure out how to specify the "x = location". Basically, we need to rename a column in loc_revenue_df to "location"
-   
-   
    
    if(graph == TRUE) {
      graph <- ggplot(loc_revenue_df) +
-       geom_col(aes(x = location, y = loc_revenue)) +
+       geom_col(aes(x = location, y = loc_revenue), fill = "darkblue") +
        labs(x = "Location", y = "Revenue ($)",
             title = "Fishing Revenue per Caribbean Country",
             subtitle = sprintf("The total revenue for fisheries within the Caribbean is $%d",
