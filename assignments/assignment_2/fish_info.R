fdff24d81f94dfd26a1432e579fa0bd96e2386dc
jbalmagia <jbalmagia@gmail.com>
2020-03-09 19:17:28 -0700

    cleaned up documentation

--- assignments/assignment_2/fish_info.R
+++ assignments/assignment_2/fish_info.R
@@ -1,12 +1,14 @@
-#'Revenue from fish catch by fishery and location
+#'Summary of Revenue for Fisheries
 #'
-#'This function calculates the frequency of fish species caught by location, total revenue by location, and total revenue by fishery
+#'This function returns revenue information about fisheries including the most frequent fish species caught at each location, revenue of all fisheries at each location, total revenue by location, and total revenue by fishery
 #'
 #'@param price a data frame that has prices for different fish
 #'@param catch a table that has the number caught for each fish species for each location, where locations are columns and fish are rows
-#'@return frequent the most frequently caught fish in each location
-#'@return rev_location the total revenue of all fisheries by location
-#'@return rev_fish the total revenue of each fishery across all locations
+#'@return Max_Catch the most frequent fish species caught at each location
+#'@return Revenue_per_Location the total revenue at each location
+#'@return Revenue_per_Fish the total revenue of each fishery across all locations
+#'@return Total_Revenue the total revenue of all fisheries at all locations
+#'@return Graph optional graph of revenue by location
 
 
 
@@ -29,11 +31,8 @@ fish_info = function(catch,price, graph = FALSE){
    fish_revenue = price[,1]*catch
    fish_revenue = rowSums(fish_revenue)
    
-#Jenny - I created the remaining equations. To output them, I had to get rid of the nested functions. I also created a new data frame with fish counts by locations (fish_loc), but the fish_max equation isn't working properly now. Sorry! Trying to troubleshoot.
-   
-# I also created the graph, but it won't run because we need to figure out how to specify the "x = location". Basically, we need to rename a column in loc_revenue_df to "location"
-   
-   
+
+#graph of revenue by location, optional if user requests
    
    if(graph == TRUE) {
      graph <- ggplot(loc_revenue_df) +
